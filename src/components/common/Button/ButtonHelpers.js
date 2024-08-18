715bb2b47ddc06fa0199f4134749408ebb81ce98
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-08-18 20:57:35 +0300

    edit button styles

--- src/components/common/Button/ButtonHelpers.js
+++ src/components/common/Button/ButtonHelpers.js
@@ -2,11 +2,10 @@ import css from "./Button.module.css";
 
 export const buildButtonClass = (type) => {
   switch (type) {
-    case "auth":
-      return css["auth-btn"];
     case "modal-window":
       return css["modal-btn"];
     default:
       return "";
+    // can be continued later as the project expands
   }
 };
