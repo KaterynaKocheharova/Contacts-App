bec6e8ca30b1b79d56ee5f95431087feb919756f
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-08-19 17:32:11 +0300

    fix modal styles - change bg color

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,15 +7,17 @@ import { useModal } from "../../hooks/UseModal";
 import css from "./Contact.module.css";
 
 export default function Contact({ contactData }) {
-  const { id, name, number } = contactData;
+  const { name, number } = contactData;
   const { modalIsOpen, openModal, closeModal } = useModal();
   const [modalType, setModalType] = useState("");
 
   const handleDeleteClick = () => {
+    setModalType("confirming deletion");
     openModal();
   };
 
   const handleUpdateClick = () => {
+    setModalType("updating");
     openModal();
   };
 
@@ -29,26 +31,16 @@ export default function Contact({ contactData }) {
           </div>
           <div className={css["contact-text-box"]}>
             <CiPhone className={css["contact-phone-icon"]} />
-            <a href={`tel: ${number}`} className={css["number-text"]}>{number}</a>
+            <a href={`tel: ${number}`} className={css["number-text"]}>
+              {number}
+            </a>
           </div>
         </div>
         <div className={css["button-box"]}>
-          <button
-            className={css["contact-btn"]}
-            onClick={() => {
-              setModalType("updating");
-              handleUpdateClick({ id, name, number });
-            }}
-          >
+          <button className={css["contact-btn"]} onClick={handleUpdateClick}>
             <FaPencilAlt className={css["button-icon"]} />
           </button>
-          <button
-            className={css["contact-btn"]}
-            onClick={() => {
-              setModalType("confirming deletion");
-              handleDeleteClick();
-            }}
-          >
+          <button className={css["contact-btn"]} onClick={handleDeleteClick}>
             <MdDeleteOutline className={css["button-icon"]} />
           </button>
         </div>
