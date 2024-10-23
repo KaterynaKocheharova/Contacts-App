c3c6d5baf0108463798072c4371ffff0dc8f5b48
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-24 00:21:01 +0300

    style toast

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -3,6 +3,11 @@ import toast from "react-hot-toast";
 const globalParams = {
   duration: 5000,
   position: "top-right",
+  style: {
+    border: "1px solid var(--first-color)",
+    color: "var(--first-color)",
+    backgroundColor: "var(--third-color)"
+  },
 };
 
 let toastId;
@@ -20,7 +25,6 @@ export const activateSuccessToast = (message) => {
   toastId = toast.success(message, globalParams);
   document.addEventListener("click", removeToastClickHandler);
 };
-
 
 export const activateErrorToast = (message) => {
   toastId = toast.error(message, globalParams);
